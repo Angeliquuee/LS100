74ea671760e1db72af16171014b75edc1562a191
Angelique Ocular <angelique.ocular@gmail.com>
2019-07-27 09:06:56 -0700

    Ex. 1: The Basics

---
+++ the_basics/exercise_1.rb
@@ -0,0 +1,15 @@
+=begin
+Add two strings together that, when concatenated,
+return your first and last name as your full name in one string.
+=end
+
+first_name = "Angelica"
+last_name = "Ocular"
+
+# String interpolation
+
+puts "#{first_name} #{last_name}"
+
+# String concatenation
+
+puts first_name + ' ' + last_name
