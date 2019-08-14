392c7f2492ee40e13811aba8f0ecdf4afa2d304b
Angelique Ocular <angelique.ocular@gmail.com>
2019-08-13 19:10:07 -0700

    Completed Methods in Ruby Basics

---
+++ ruby_basics/methods/make_and_model.rb
@@ -0,0 +1,8 @@
+# Using the following code, write a method called car that takes two arguments
+# prints a string containing the values of both arguments.
+
+def car(brand, model)
+  puts "#{brand} #{model}"
+end
+
+car('Toyota', 'Corolla')
